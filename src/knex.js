e5d7e53727d02d5cb7ba6b729098054cde575638
aqshol afifi <aqsholafifi@gmail.com>
2023-02-20 14:55:15 +0700

    Update knex.js

--- src/knex.js
+++ src/knex.js
@@ -3,7 +3,7 @@ module.exports = {
     connection: {
       host: 'localhost',
       user: 'root',
-      password: '',
+      password: 'mike429A',
       database: 'gols_antarin_db',
       charset: 'utf8',
     },
